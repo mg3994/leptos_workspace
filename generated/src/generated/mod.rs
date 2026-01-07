631759289b5b932089020f7d4c7dec214f0ef508
mg3994 <manishgautammg7@yahoo.com>
2026-01-07 10:51:42 -0800

    grpc idea dropped : don't remove packages

--- generated/src/generated/mod.rs
+++ generated/src/generated/mod.rs
@@ -1,3 +1,4 @@
 pub mod clock;
+pub mod echo;
 pub mod payments;
 pub mod routeguide;
--- server/src/main.rs
+++ server/src/main.rs
@@ -8,8 +8,31 @@ use leptos_axum::{generate_route_list, LeptosRoutes};
 use app::*;
 use leptos::logging::log;
 
+// pub mod generated{
+//     pub mod echo {
+//         tonic::include_proto!("echo"); // Ensure this matches your package name in .proto
+//     }
+// }
+#[derive(Debug, Clone, Default)]
+pub struct MyEcho;
+
+#[tonic::async_trait] // <--- This is required
+impl generated::echo::echo_server::Echo for MyEcho {
+    async fn echo(
+        &self,
+        request: tonic::Request<generated::echo::EchoRequest>,
+    ) -> Result<tonic::Response<generated::echo::EchoReply>, tonic::Status> {
+        Ok(tonic::Response::new(generated::echo::EchoReply {
+            message: format!("Echoing back: {}", request.get_ref().message),
+        }))
+    }
+}
+
+
 #[tokio::main]
 async fn main() {
+    // Load .env files.
+    dotenvy::dotenv().ok();
 
     let conf = get_configuration(None).unwrap();
     let addr = conf.leptos_options.site_addr;
@@ -17,7 +40,7 @@ async fn main() {
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(App);
 
-    let app = Router::new()
+    let axum_make_service = Router::new()
         .leptos_routes(&leptos_options, routes, {
             let leptos_options = leptos_options.clone();
             move || shell(leptos_options.clone())
@@ -25,11 +48,19 @@ async fn main() {
         .fallback(leptos_axum::file_and_error_handler(shell))
         .with_state(leptos_options);
 
+    // Correct way to initialize the service
+    let echo_impl = MyEcho::default();
+    let echo_service = generated::echo::echo_server::EchoServer::new(echo_impl);
+
+    let grpc_router = tonic::transport::Server::builder()
+        .add_service(echo_service);
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     log!("listening on http://{}", &addr);
     let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
-    axum::serve(listener, app.into_make_service())
+    axum::serve(listener, axum_make_service.into_make_service())
         .await
         .unwrap();
 }
+
+
